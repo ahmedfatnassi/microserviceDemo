00232de96bb225bc2789d4adedfb324faae285a4
Amed FATNASSi (ENISo) <ahmed.fatnassi@eniso.u-sousse.tn>
2020-10-29 22:59:32 +0100

    externalized Configuration added

--- second/src/main/java/com/secondapp/microservice/second/SecondApplication.java
+++ second/src/main/java/com/secondapp/microservice/second/SecondApplication.java
@@ -2,7 +2,6 @@ package com.secondapp.microservice.second;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 public class SecondApplication {
